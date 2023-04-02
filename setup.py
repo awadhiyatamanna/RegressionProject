6b5f0186ce9d68ef941403cb769ee8cc20c71607
awadhiyatamanna <awadhiyatamanna@gmail.com>
2023-04-02 17:16:22 +0530

    second commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT = "-e ."
+HYPHEN_E_DOT = "-e ."
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
@@ -9,8 +9,8 @@ def get_requirements(file_path:str)->List[str]:
         requirements=[req.replace("\n","") for req in requirements]
 
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove("HYPEN_E_DOT")
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
     
     return requirements
